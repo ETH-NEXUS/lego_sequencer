3996eb38c281750606da0ede8c039786620ba3a6
faisala <alquaddoomi@nexus.ethz.ch>
2019-08-27 01:27:25 +0900

    Added comments to the settings, disabled mocking.

--- sequencer/default_settings.py
+++ sequencer/default_settings.py
@@ -1,4 +1,20 @@
-MOCK_COMM = True  # if true, mocks the communication with the LEGO brick
-TIME_MOD = 0.2  # if MOCK_COMM=True, the process duration is multiplied by this value (>1.0 slower, <1.0 faster)
-MOCK_BLAST = True  # if true, mocks the NCBI blast request process and returns a canned result
+# -----------------------------------
+# --- LEGO Sequencer Mocking
+# -----------------------------------
+
+# if true, mocks communication with the LEGO brick, producing sequences from real organisms
+# (see support.ev3_reader.SAMPLE_SEQUENCES for details)
+MOCK_COMM = False
+# if MOCK_COMM=True, the mock process duration is multiplied by this value (>1.0 slower, <1.0 faster)
+TIME_MOD = 0.5
+
+
+# -----------------------------------
+# --- NCBI BLAST Mocking:
+# -----------------------------------
+
+# if true, mocks the NCBI blast request process and returns a canned result
+MOCK_BLAST = False
+# if MOCK_BLAST and MOCK_BLAST_HAS_RESULTS are true, returns a canned file with hits;
+# otherwise, returns a canned file with no hits
 MOCK_BLAST_HAS_RESULTS = True
